6fdb6398f98e7a622a776da00bbf3fa924d39b1d
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2025-07-28 16:11:04 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "vamsi-buckt"
     key            = "ec2/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table" # Optional
   }
 }
