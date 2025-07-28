f4fb90b027e011bb295c9bc74a08560249f80291
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2025-07-28 15:51:54 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
